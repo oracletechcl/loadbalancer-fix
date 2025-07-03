a265db97aa46bd2f3f57b8f63c580906df22c530
Denny Alquinta <dralquinta@gmail.com>
2025-07-03 19:40:52 +0000

    Fixes certs

--- fix-original-ingress-controller/ingress-controller/test/03-create-certificate.sh
+++ fix-original-ingress-controller/ingress-controller/test/03-create-certificate.sh
@@ -1,1 +1,2 @@
 kubectl create secret tls wildcard-backend-tls   --namespace=qa-progreso-namespace   --cert=wildcard_duoc_cl.crt   --key=wildcard_duoc_cl.key
+kubectl create secret tls wildcard-backend-tls   --namespace=ingress-nginx   --cert=wildcard_duoc_cl.crt   --key=wildcard_duoc_cl.key 
